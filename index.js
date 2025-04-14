8671c5bcb6a2c73afa4583153095f3af63b6045d
Juan Centenaro <87022082+jcentenaro@users.noreply.github.com>
2025-04-14 08:04:02 -0300

    path para VERCEL con require

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
 const express = require("express");
+const expressEjsLayouts = require("express-ejs-layouts");
+const path = require('path') 
 const app = express();
-
 const expressLayouts = require("express-ejs-layouts");
 
 
-
-app.use(express.static("public"));
+//Modifico para VERCEL
+// app.use(express.static("public"));
+app.use(express.static(path.join(__dirname, "/public")));
 
 app.set("view engine", "ejs");
-app.set("views", "./src/views")
+//Modifico para VECEL
+// app.set("views", "./src/views")
+app.set("views", path.join(__dirname, "/src/views"));
 
 app.use(expressLayouts);
 app.set("layout", "./layouts/layout")
