7deb912d138d6298ee0e27a22880aa9dda57e0fc
Juan Centenaro <87022082+jcentenaro@users.noreply.github.com>
2025-04-14 07:42:46 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const app = express();
+
+const expressLayouts = require("express-ejs-layouts");
+
+
+
+app.use(express.static("public"));
+
+app.set("view engine", "ejs");
+app.set("views", "./src/views")
+
+app.use(expressLayouts);
+app.set("layout", "./layouts/layout")
+
+app.use(express.urlencoded({ extended: false}));
+
+app.use(require("./src/routes/admin/tareasRoute"))
+
+const PORT = 3000
+app.listen(PORT, () => console.log(`http://localhost:${PORT}`));
