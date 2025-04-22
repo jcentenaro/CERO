d1274efa8e9d4d545d1a2d2240211b010ed51155
Juan Centenaro <87022082+jcentenaro@users.noreply.github.com>
2025-04-22 19:32:18 -0300

    cambio layout en admin

--- src/controllers/admin/adminController.js
+++ src/controllers/admin/adminController.js
@@ -1,157 +1,144 @@
-const fs = require("fs"); //permite borrar las imagenes de los productos borrados
+const fs = require("fs");
 const path = require("path");
-const sharp = require("sharp"); // Para controlar los cambios en las imagenes antes de guardarlas
-// LLamo a express-validator del Rroutes
+const sharp = require("sharp");
 const { validationResult } = require("express-validator");
-const { error } = require("console");
-
-const model = require("../../models/product")
+const model = require("../../models/product");
 
 const index = async (req, res) => {
-  try {
-    const productos = await model.findAll();
-    console.log(productos);
-    res.render("admin/index", { productos });
-  } catch (error) {
-    console.log(error)
-    res.status(500).send(error);
-  }
+    try {
+        const productos = await model.findAll();
+        res.render("admin/index", { 
+            productos, 
+            layout: "layouts/layoutadmin" // Especifica el layout de administración
+        });
+    } catch (error) {
+        console.log(error);
+        res.status(500).send(error);
+    }
 };
 
 const create = (req, res) => {
-  res.render("admin/create")
-}
+    res.render("admin/create", { 
+        layout: "layouts/layoutadmin" // Especifica el layout de administración
+    });
+};
 
 const store = async (req, res) => {
-  console.log(req.body, req.file); // agrego req.file para que requiera el archivo de imagen
+    const errors = validationResult(req);
 
-  const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.render("admin/create", {
+            values: req.body,
+            errors: errors.array(),
+            layout: "layouts/layoutadmin" // Especifica el layout de administración
+        });
+    }
 
-  if(!errors.isEmpty()) {
-    return res.render("admin/create", {
-      values: req.body, //mantengo los datos de campos bien cargados
-      errors: errors.array(),
-    });
-  }
-  //Guardo la info en BD
-  try {
-    const producto = await model.create(req.body);
-    console.log(producto);
+    try {
+        const producto = await model.create(req.body);
 
-  // USO SHARP con validaciones y detalles de cómo quiero que guarde la imagen
-  if (producto && req.file) {
-  sharp(req.file.buffer)
-  .resize
-  (800, 1200, {
-    kernel: sharp.kernel.nearest,
-    fit: 'contain',
-    position: 'center',
-    background: { r: 232, g: 232, b: 232, alpha: 0.5 }
-  })
-  .toFile(path.resolve(__dirname, `../../../public/uploads/productos/producto_${producto.id}.jpg`));
-  }
+        if (producto && req.file) {
+            sharp(req.file.buffer)
+                .resize(800, 1200, {
+                    kernel: sharp.kernel.nearest,
+                    fit: "contain",
+                    position: "center",
+                    background: { r: 232, g: 232, b: 232, alpha: 0.5 }
+                })
+                .toFile(path.resolve(__dirname, `../../../public/uploads/productos/producto_${producto.id}.jpg`));
+        }
 
-  res.redirect('/admin/productos')
-} catch (error) {
-  console.log(error);
-  res.status(500).send(error);
-}
+        res.redirect("/admin/productos");
+    } catch (error) {
+        console.log(error);
+        res.status(500).send(error);
+    }
 };
 
 const edit = async (req, res) => {
-  try {
-    const producto = await model.findByPk(req.params.id)
+    try {
+        const producto = await model.findByPk(req.params.id);
 
-    if (producto) {
-      res.render("admin/edit", { values: producto });
-    } else {
-      res.status(404).send("El producto no existe");
+        if (producto) {
+            res.render("admin/edit", { 
+                values: producto, 
+                layout: "layouts/layoutadmin" // Especifica el layout de administración
+            });
+        } else {
+            res.status(404).send("El producto no existe");
+        }
+    } catch (error) {
+        console.log(error);
+        res.status(500).send(error);
     }
-  } catch (error) {
-    console.log(error);
-    res.status(500).send(error);
-  }
 };
 
 const update = async (req, res) => {
-  console.log(req.params, req.body);
+    const errors = validationResult(req);
 
-  const errors = validationResult(req);
-
-  if(!errors.isEmpty()) {
-    return res.render("admin/edit", {
-      values: req.body, //mantengo los datos de campos bien cargados
-      errors: errors.array(),
-    });
-  }
-
-  try {
-    const affected = await model.update(req.body, {
-      where: {
-        id: req.params.id,
-      },
-    });
-
-    if (affected[0] == 1) {
-      if (req.file) {
-        sharp(req.file.buffer)
-        .resize
-        (400, 600, {
-          kernel: sharp.kernel.nearest,
-          fit: 'contain',
-          position: 'center',
-          background: { r: 232, g: 232, b: 232, alpha: 0.5 }
-        })
-        .toFile(path.resolve(__dirname, `../../../public/uploads/productos/producto_${req.params.id}.jpg`)); 
-      }
-      res.redirect("/admin/productos");
-    } else {
-      res.status(500).send("Error al actualizar");
+    if (!errors.isEmpty()) {
+        return res.render("admin/edit", {
+            values: req.body,
+            errors: errors.array(),
+            layout: "layouts/layoutadmin" // Especifica el layout de administración
+        });
     }
-  } catch (error) {
-    console.log(error);
-    res.status(500).send(error);
-  }
+
+    try {
+        const affected = await model.update(req.body, {
+            where: { id: req.params.id }
+        });
+
+        if (affected[0] === 1) {
+            if (req.file) {
+                sharp(req.file.buffer)
+                    .resize(400, 600, {
+                        kernel: sharp.kernel.nearest,
+                        fit: "contain",
+                        position: "center",
+                        background: { r: 232, g: 232, b: 232, alpha: 0.5 }
+                    })
+                    .toFile(path.resolve(__dirname, `../../../public/uploads/productos/producto_${req.params.id}.jpg`));
+            }
+            res.redirect("/admin/productos");
+        } else {
+            res.status(500).send("Error al actualizar");
+        }
+    } catch (error) {
+        console.log(error);
+        res.status(500).send(error);
+    }
 };
 
 const destroy = async (req, res) => {
-  console.log(req.params);
+    try {
+        const result = await model.destroy({
+            where: { id: req.params.id }
+        });
 
-  try {
-    const result = await model.destroy({
-      where: {
-        id: req.params.id,
-      },
-    });
-    console.log(result);
-
-    if (result === 1) {
-      try {
-        await fs.promises.unlink(
-          path.resolve(
-            __dirname,
-            `../../../public/uploads/productos/producto_${req.params.id}.jpg`
-          )
-        );
-      } catch (fileError) {
-        console.log("No se encontró la imagen para eliminar o ya fue eliminada:", fileError);
-        // Continúa incluso si la imagen no existe
-      }
-      res.redirect("/admin/productos");
-    } else {
-      res.status(404).send("El producto no existe");
+        if (result === 1) {
+            try {
+                await fs.promises.unlink(
+                    path.resolve(__dirname, `../../../public/uploads/productos/producto_${req.params.id}.jpg`)
+                );
+            } catch (fileError) {
+                console.log("No se encontró la imagen para eliminar o ya fue eliminada:", fileError);
+            }
+            res.redirect("/admin/productos");
+        } else {
+            res.status(404).send("El producto no existe");
+        }
+    } catch (error) {
+        console.log(error);
+        res.status(500).send(error);
     }
-  } catch (error) {
-    console.log(error);
-    res.status(500).send(error);
-  }
 };
 
 module.exports = {
-  index,
-  create,
-  store,
-  edit,
-  update,
-  destroy,
+    index,
+    create,
+    store,
+    edit,
+    update,
+    destroy
 };
